549e9f1bdaf2ebf4dabb3742c4276f49b4c81fdc
TenorioDan <tenroiod@uci.edu>
2014-10-18 13:17:39 -0700

    Daniel's Code
    Added my unity project to the repository

---
+++ New Unity Project/Assets/Tiles/TileScripts/TileScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileScript : MonoBehaviour {
+
+	public bool occupied = false;
+	public GameObject environmentObject;
+		
+	public GameObject left, right, down, up;
+	public GameObject objectOccupyingTile;
+
+	// Use this for initialization
+	void Start () {
+
+		this.GetComponent<BaseObject>().type = BaseObject.ObjectType.Tile;
+		int random = Random.Range (0, 100);
+
+		if (random < 10) 
+		{
+			GameObject tree = (GameObject)Instantiate(environmentObject, 
+											            new Vector3(this.transform.position.x, 0, this.transform.position.z), 
+											            new Quaternion());
+			tree.transform.parent = this.transform;
+			objectOccupyingTile = tree;
+			renderer.material.color = Color.red;
+			this.occupied = true;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+}
