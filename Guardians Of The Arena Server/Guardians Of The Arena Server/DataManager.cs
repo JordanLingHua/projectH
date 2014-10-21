0471152f444340efdebde115a340a655138372a3
TenorioDan <tenroiod@uci.edu>
2014-10-21 12:21:57 -0700

    Added Server Code (For reals this time)

---
+++ Guardians Of The Arena Server/Guardians Of The Arena Server/DataManager.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Net;
+using System.Text;
+
+
+namespace Guardians_Of_The_Arena_Server
+{
+    public class DataManager
+    {
+        public static SQLiteConnection userDatabase;
+        IPEndPoint RemoteEndPoint;
+        Socket s;
+
+
+
+        public DataManager()
+        {
+            RemoteEndPoint = new IPEndPoint(IPAddress.Any, 9000);
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //createDatabase();
+            connectToDatabase();
+            //createTables();
+            //clearTable();
+            //fillPlayerTable();
+            printTable();
+
+        }
+
+        // Creates an empty database
+
+        void createDatabase()
+        {
+            try
+            {
+
+               SQLiteConnection.CreateFile("GOA_User_Database.sqlite");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        // Creates a connection with our database
+
+        void connectToDatabase()
+        {
+            userDatabase = new SQLiteConnection("Data Source=GOA_User_Database.db;Version=3;");
+            userDatabase.Open();
+        }
+
+        // Create two tables
+
+        // a table named 'playerInfo' with two columns: nae (a string of max 20 characters) and password(a string of max 50 characters)
+        // a table named 'highscores' with two columns: name (a string of max 20 characters) and score (an int)
+
+        void createTables()
+        {
+            string sql = "create table playerInfo (name varchar(20), password varchar(50))";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            command.ExecuteNonQuery();
+            sql = "create table highScores (name varchar(20), score int)";
+            command = new SQLiteCommand(sql, userDatabase);
+            command.ExecuteNonQuery();
+        }
+
+        // Clear the tables
+
+        public void clearTable()
+        {
+            string sql = "DELETE FROM playerInfo";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            SQLiteDataReader reader = command.ExecuteReader();
+            sql = "DELETE FROM highScores";
+            command = new SQLiteCommand(sql, userDatabase);
+            reader = command.ExecuteReader();
+        }
+
+        // print the tables to the console
+
+        public void printTable()
+        {
+            string sql = "select * from playerInfo";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["name"] + "\tpassword: " + reader["password"]);
+        }
+
+        // check to see if a value exist in the table
+
+        public bool existsInTable(String name)
+        {
+            string sql = "SELECT count(*) FROM playerInfo WHERE name=:Name";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            command.Parameters.AddWithValue(":Name", name);
+            int count = Convert.ToInt32(command.ExecuteScalar());
+
+            return (count != 0);
+        }
+
+        // get the user's password from the playerInfo database
+
+        public string getUserPassword(String name)
+        {
+            string sql = "select password from playerInfo where name=" + "'" + name + "'";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            SQLiteDataReader reader = command.ExecuteReader();
+            return reader["password"].ToString();
+        }
+
+
+
+
+        public void getTableEntry(String name)
+        {
+            string sql = "select * from playerInfo where name=" + "'" + name + "'";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            SQLiteDataReader reader = command.ExecuteReader();
+            Console.WriteLine(reader["name"] + " : " + reader["password"]);
+
+        }
+
+        // insert a player into the playerInfo table
+
+        public void insertIntoPlayer(string name, string password)
+        {
+            string sql = "select * from playerInfo where name =:name";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            command.Parameters.AddWithValue(":name", name);
+            SQLiteDataReader dataReader = command.ExecuteReader();
+
+            // if the name is in the database
+
+            if (dataReader.Read())
+            {
+                Console.WriteLine("found: " + name);
+                if (password.Equals(dataReader["password"]))
+                {
+                    Console.WriteLine("you have entered the correct password");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid passowrd");
+                }
+            }
+            else
+            {
+                sql = "insert into playerInfo (name, password) values(@param1, @param2)";
+                command = new SQLiteCommand(sql, userDatabase);
+                command.Parameters.Add(new SQLiteParameter("@param1", name));
+                command.Parameters.Add(new SQLiteParameter("@param2", password));
+                command.ExecuteNonQuery();
+
+            }
+
+        }
+
+        public void insertIntoHighScores(string name, int score)
+        {
+            string sql = "insert into highScores (name, score) values(@param1, @param2)";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            command.Parameters.Add(new SQLiteParameter("@param1", name));
+            command.Parameters.Add(new SQLiteParameter("@param2", score));
+            command.ExecuteNonQuery();
+        }
+
+        public void deleteFromHighScores(string name)
+        {
+            string sql = "delete from highScores WHERE name='" + name + "'";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            command.ExecuteNonQuery();
+        }
+
+        public void printHighTable()
+        {
+
+            string sql = "select * from highScores";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+        }
+
+        public void updateHighScores(string name, int score)
+        {
+            string sql = "UPDATE highScores SET score=@newScore WHERE name=@name";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@newScore", score);
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (Exception fail)
+            {
+                Console.WriteLine(fail.Message);
+            }
+
+
+            // if the name is in the database
+
+
+
+        }
+
+        public void sendPacket(String action, String name, int score)
+        {
+            Byte[] data = Encoding.ASCII.GetBytes(String.Format("{0}/{1}/{2}", action, name, score));
+            s.SendTo(data, data.Length, SocketFlags.None, RemoteEndPoint);
+        }
+
+
+
+
+
+        // Inserts some values in the highscores table.
+        // As you can see, there is quite some duplicate code here, we'll solve this in part two.
+
+        void fillPlayerTable()
+        {
+            string sql = "insert into playerInfo  (name, password) values ('Jay', '39ec785d60a1b23bfda9944b9138bbcf')";
+            SQLiteCommand command = new SQLiteCommand(sql, userDatabase);
+            command.ExecuteNonQuery();
+            sql = "insert into playerInfo  (name, password) values ('Me', 3232 )";
+            command = new SQLiteCommand(sql, userDatabase);
+            command.ExecuteNonQuery();
+            sql = "insert into playerInfo  (name, password) values ('Not me', 30011)";
+            command = new SQLiteCommand(sql, userDatabase);
+            command.ExecuteNonQuery();
+        }
+
+
+
+
+    }
+}
+
---
+++ Guardians Of The Arena Server/Guardians Of The Arena Server/Server.cs
@@ -0,0 +1,433 @@
+﻿using System;
+//using System.Drawing;
+using System.Diagnostics;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Data.SQLite;
+
+namespace Guardians_Of_The_Arena_Server
+{
+    class Server
+    {
+        public static DataManager dm;
+
+        protected static int numberOfClients;
+
+        protected static Random rng;
+
+        protected static TcpListener listener;
+
+        protected static Socket[] socArray;
+        protected static Client[] clientArray;
+        protected static List<String> loginNames;
+
+
+        protected static Stopwatch uniClock;
+        protected static Stopwatch commandClock;
+
+        protected static float TopBorderPosition;
+        protected static float BottomBorderPosition;
+        protected static float RightBorderPosition;
+        protected static float LeftBorderPosition;
+
+        protected static bool playing = false;
+
+        protected ServerLoop loop;
+
+        public Thread listenerThead;
+
+
+        public Server()
+        {
+
+            dm = new DataManager();
+            numberOfClients = 0;
+
+            loginNames = new List<string>();
+ 
+
+            TopBorderPosition = RightBorderPosition = 5.0f;
+            BottomBorderPosition = LeftBorderPosition = -5.0f;
+
+            rng = new Random();
+            listener = new TcpListener(4188);
+            socArray = new Socket[5];
+            clientArray = new Client[5];
+
+            listenerThead = new Thread(new ThreadStart(this.Listen));
+
+            uniClock = new Stopwatch();
+            commandClock = new Stopwatch();
+
+            loop = new ServerLoop(this);
+            loop.loopThread.Start();
+
+        }
+
+
+
+        public void Listen()
+        {
+            listener.Start();
+
+            while (true)
+            {
+
+                //accept an incoming request to make a connection then return 
+                //the socket(endpoint) associate with the connection 
+                Socket soc = listener.AcceptSocket();
+                socArray[numberOfClients] = soc;
+
+
+                Console.WriteLine("Connected: {0}", soc.RemoteEndPoint);//print connection details
+                try
+                {
+                    NetworkStream nws = new NetworkStream(soc);
+                    StreamReader sr = new StreamReader(nws);//return the stream to read from
+                    StreamWriter sw = new StreamWriter(nws); //establish a stream to read to
+                    sw.AutoFlush = true;//enable automatic flushing, flush thte wrtie stream after every write command, no need to send buffered data
+
+                    int clientNumber = numberOfClients;
+
+                    Client client = new Client(this, nws, sr, sw, clientNumber);
+                    client.thread.Start();
+
+                    clientArray[numberOfClients++] = client ;
+
+                    //loop.playerConnected();
+
+                    Console.WriteLine("Client " + client.clientNumber + " has connected");
+                    Console.WriteLine("There are now " + numberOfClients + " clients");
+                    client.sw.WriteLine("client\\" + client.clientNumber);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
+            }
+
+        }
+
+
+        //if a client is disconnected in anyway
+        //we must add that client number to the list of available numbers and remove the pointer to this client
+        //and inform all other clients that this client has disconnected
+        public void RemoveClient(Client client)
+        {
+            client.thread.Abort();
+            numberOfClients--;
+
+
+            Console.WriteLine("Name about to be removed {0}", client.clientName);
+            loginNames.Remove(client.clientName);
+            //dm.deleteFromHighScores(client.clientName);
+
+            foreach (Client c in clientArray)
+            {
+                c.sw.WriteLine("disconnected\\" + client.clientNumber);
+            }
+
+            Console.WriteLine("There are now " + numberOfClients + " clients");
+        }
+
+        public class ServerLoop
+        {
+            private Server serverRef;
+            public Thread loopThread;
+
+            public ServerLoop(Server serverRef)
+            {
+                this.serverRef = serverRef;
+                loopThread = new Thread(new ThreadStart(this.loop));
+                uniClock.Start();
+            }
+
+            //recursively check if all clients are ready
+            //public bool ClientsReady(LinkedList<Client> clients, LinkedListNode<Client> currentClient)
+            //{
+
+
+            //    if (currentClient.Next == null)
+            //        return currentClient.Value.clientReady;
+            //    else
+            //        return currentClient.Value.clientReady && ClientsReady(clientArray, currentClient.Next);
+            //}
+
+
+            //if the game has already started we do not need to check if the clients are ready
+            //simply tell the client that has just connected the game info
+            public void loadGameState(Client client)
+            {
+                
+            }
+
+            //server loop that checks messages from clients
+            public void loop()
+            {
+
+                //always run this loop
+                while (true)
+                {
+                    try
+                    {
+                        //if all connected clients are read, we can start the game
+                        //if (numberOfClients > 1 && ClientsReady(clientArray, clientArray.First))
+                        //{
+                        //    //create all the pellets on the server
+
+                        //    foreach (Client c in clientArray)
+                        //    {
+                        //        c.clientReady = false;
+
+                        //        //let all the clients know about each other
+                        //        foreach (Client c2 in clientArray)
+                        //        {
+                        //            c2.sw.WriteLine("connected\\" + c.clientNumber);
+
+                        //        }
+
+                        //        //now that all the clients have the initial game state
+                        //        //each client can start playing
+                        //        c.sw.WriteLine("start");
+                        //    }
+
+                        //    //now that the players have started playing
+                        //    //the server should now start checking if any collisions have occured 
+                        //    //collisionThread.Start();
+                        //    commandClock.Start();
+                        //    playing = true;
+                        //}
+
+                        //check each client in the array
+                        //if they have commands waiting in the queue, dequeue and execute that command
+
+
+
+                        for (int i = 0; i < numberOfClients; i++)
+                        {
+                            Client client = clientArray[i];
+
+                            if (client == null)
+                                Console.WriteLine("CLIENT " + i + " IS NULL BECAUSE OF THREADING");
+
+                            //the server must check if it has recieved any commands from the clients
+                            if (client.commandQueue.Count > 0)
+                            {
+                                //each command is a string that will be delimited by \\
+                                string command = client.commandQueue.Dequeue();
+
+                                //each message will be broken into an array of strings
+                                string[] tokens = command.Split(new string[] { "\\" }, StringSplitOptions.None);
+
+                                /*
+                                 *the commands are as follows
+                                 *play - tells the server that the client is ready to start playing the game
+                                 *userInfo - tells the server that the client is sending login information
+                                 *      -check sthe database and either logins the clients, creates new user info,
+                                 *       denies the login attemps
+                                 *velocity - broadcasts the sending client's updated velocity
+                                 *position - broadcasts the sending client's updated position
+                                 *score - broadcasts the correct scores to each client
+                                 *lag - adds a delay when sending messages between client and server (for testing purposes)
+                                 */
+
+                                if (tokens[0].Equals("play"))
+                                {
+                                    if (playing)
+                                    {
+                                        loadGameState(client);
+                                    }
+                                    else
+                                    {
+                                        client.clientReady = true;
+                                        Console.WriteLine("client " + client.clientNumber + " is ready");
+                                    }
+                                }
+
+
+                                //
+                                else if (tokens[0].Equals("userInfo"))
+                                {
+
+                                    if (!loginNames.Contains(tokens[1]))
+                                    {
+
+                                        if (dm.existsInTable(tokens[1]))
+                                        {
+                                            if (tokens[2].Equals(dm.getUserPassword(tokens[1])))
+                                            {
+                                                client.sw.WriteLine("loginSucceed\\" + tokens[1]);
+                                                client.sw.WriteLine("hasLoggedIn\\" + tokens[1]);
+                                                client.clientName = tokens[1];
+                                                Console.WriteLine(tokens[1] + " has logged in");
+                                                dm.printTable();
+
+
+                                                for (int j = 0; j < numberOfClients; j++)
+                                                {
+                                                    Client clientToSend = clientArray[j];
+
+                                                    if (client.clientNumber != clientToSend.clientNumber)
+                                                    {
+                                                        client.sw.WriteLine("hasLoggedIn\\" + clientToSend.clientName);
+                                                        clientToSend.sw.WriteLine("hasLoggedIn\\" + client.clientName);
+                                                    }
+                                                }
+                                               
+                                            }
+                                            else
+                                            {
+                                                client.sw.WriteLine("loginFail");
+                                                Console.WriteLine(tokens[1] + " entered incorrect password");
+                                            }
+                                        }
+
+                                        else
+                                        {
+                                            dm.insertIntoPlayer(tokens[1], tokens[2]);
+                                            loginNames.Add(tokens[1]);
+                                            client.sw.WriteLine("loginSucceed\\" + tokens[1]);
+                                            client.sw.WriteLine("hasLoggedIn\\" + tokens[1]);
+                                            client.clientName = tokens[1];
+                                            // dm.insertIntoHighScores(client.clientName, client.score);
+                                            dm.printTable();
+
+    //-------------------------REMOVE COPY AND PASTED CODE! CLEAN UP! ASAP! --------------------------------------------------//
+
+                                            for (int j = 0; j < numberOfClients; j++)
+                                            {
+                                                Client clientToSend = clientArray[j];
+
+                                                if (client.clientNumber != clientToSend.clientNumber)
+                                                {
+                                                    client.sw.WriteLine("hasLoggedIn\\" + clientToSend.clientName);
+                                                    clientToSend.sw.WriteLine("hasLoggedIn\\" + client.clientName);
+                                                }
+                                            }
+                                        }
+
+
+                                    }
+                                    else
+                                    {
+                                        client.sw.WriteLine("alreadyLoggedIn\\" + tokens[1]);
+                                    }
+
+                                }
+
+                                else if (tokens[0].Equals("logout"))
+                                {
+                                    serverRef.RemoveClient(client);
+                                    //Console.WriteLine("Name about to be removed {0}", tokens[1]);
+                                    //loginNames.Remove(tokens[1]);
+                                    //dm.deleteFromHighScores(client.clientName);
+                                    //dm.sendPacket("remove", client.clientName, client.score);
+
+                                }
+                                else if (tokens[0].Equals("disconnect"))
+                                {
+                                    //dm.sendPacket("remove", client.clientName, client.score);
+                                }
+                                else
+                                {
+                                    //do nothing
+                                }
+
+                            }
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+
+                        Console.WriteLine(e.StackTrace);
+                        Console.WriteLine(e.Message);
+
+                    }
+                }
+            }
+
+
+
+        }
+
+        //client class that defines all the attributes 
+        public class Client
+        {
+            private Server serverRef;
+            public NetworkStream nws;
+            public StreamReader sr;
+            public StreamWriter sw;
+            public Queue<string> commandQueue;
+            public Thread thread;
+
+            public int clientNumber;
+            public string clientName;
+
+            public bool clientReady = false;
+            public bool spawned = false;
+
+            //each client must keep track of the network stream between itself and the server
+            //the stream reader used by the server to read in client messages
+            //the stream writer used by the server to send the client messages
+            //an assigned client number
+            public Client(Server serverRef, NetworkStream nws, StreamReader sr, StreamWriter sw, int clientNumber)
+            {
+                this.serverRef = serverRef;
+                this.nws = nws;
+                this.sr = sr;
+                this.sw = sw;
+                this.clientNumber = clientNumber;
+
+                //each client has a thread that the server uses to alternate reading in messages from
+                thread = new Thread(new ThreadStart(this.Service));
+                commandQueue = new Queue<string>();
+            }
+
+
+            //we will collisions based on the position of the walls
+            //if any of those conditions are true then it is impossible for a collision to have occured
+
+
+
+
+            //checks if the client has sent any messages
+            public void Service()
+            {
+
+                try
+                {
+                    while (true)
+                    {
+
+                        string data = sr.ReadLine();
+
+                        lock (commandQueue)
+                        {
+                            commandQueue.Enqueue(data);
+                        }
+                    }
+                }
+
+                catch (Exception e)
+                {
+                    //Console.WriteLine(e.InnerException);
+                    //If the steam closes we will catch the exception and remove the client from the game
+                    //properly
+                    //Server.RemoveClient(this);
+                    //Console.WriteLine(e.StackTrace);
+                    serverRef.RemoveClient(this);
+                    Console.WriteLine(e.Message + " Client " + this.clientNumber + " has disconnected");
+
+                }
+            }
+        }
+    }
+}
