7cfe746f83f891d181f8bd43f259e69061126b9e
JordanLingHua <jordan.hua@yahoo.com>
2014-10-28 20:19:25 -0700

    Merge branch 'master' of https://github.com/JordanLingHua/projectH
    Conflicts:
    Guardians of the Arena/Guardians of the Arena-csharp.sln
    Guardians of the Arena/Guardians of the Arena.sln
    Guardians of the Arena/Guardians of the Arena.userprefs
    Guardians of the Arena/Library/CurrentLayout.dwlt
    Guardians of the
    Arena/Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll
    Guardians of the
    Arena/Library/ScriptAssemblies/Assembly-CSharp-firstpass.dll.mdb
    Guardians of the Arena/Library/ScriptAssemblies/Assembly-CSharp.dll
    Guardians of the Arena/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    Guardians of the
    Arena/Library/ScriptAssemblies/Assembly-UnityScript-firstpass.dll
    Guardians of the
    Arena/Library/ScriptAssemblies/Assembly-UnityScript-firstpass.dll.mdb
    Guardians of the Arena/Library/assetDatabase3
    Guardians of the
    Arena/Library/metadata/4a/4a26b53029fd6bc4d82275fa6b579e19
    Guardians of the
    Arena/Library/metadata/4a/4a8964fb15da5df4a9ffefe51655950c
    Guardians of the
    Arena/Library/metadata/89/89fb36aa5f85ec0469388bf528f6501c
    Guardians of the
    Arena/Library/metadata/9c/9c25cd0a57202d34d83908160b4fc189
    Guardians of the Arena/Library/shadercompiler-1.log

---
+++ Guardians of the Arena/Assets/PlayerSetup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSetup : MonoBehaviour {
+
+
+	//Essentially a class containing the Physical Player itself. 
+	//Should be called "player profile" actually.  
+
+	public int purchasingLimit = 10;
+
+	public GameObject[,] providedPieces;
+
+	//Add to this array everytime a piece is dropped onto a board
+	public GameObject[,] playerPieces;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	//Drag and drop
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
