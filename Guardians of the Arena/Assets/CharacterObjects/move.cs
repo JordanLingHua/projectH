a271e596eef3af5216b0ad562076341e0202e70a
joester <joey_shepard@sbcglobal.net>
2014-11-18 14:57:41 -0800

    new move file
    still to be tested on server

--- Guardians of the Arena/Assets/CharacterObjects/move.cs
+++ Guardians of the Arena/Assets/CharacterObjects/move.cs
@@ -4,7 +4,9 @@ using System.Collections;
 
 public class move : MonoBehaviour {
 
-
+	GameProcess gp;
+	bool pieceMoved;
+	bool onField;
 	public PlayerSetup playerSetup;
 	SetupTileScript oldScript;
 	//drag and drop
@@ -14,151 +16,133 @@ public class move : MonoBehaviour {
 	//Needed to avoid getting the dragged object stuck when mouse is released up
 	private bool isTouched = false;
 
-	//nearest slot; will keep UPDATING!!!!
-	Transform slot;
+	GameObject nearestTile;
 
 	//slot tag's name 
 	public string tagName = "Tile";
 
-
 	void Start()
 	{
+		gp = GameObject.Find ("GameProcess").GetComponent<GameProcess>();
+		pieceMoved = false;
 		playerSetup = GameObject.Find ("PlayerSetup").GetComponent<PlayerSetup>(); 
 	}
 
-
 	void OnMouseDown()
 	{
-		//
-		//PlayerSetup.prevPosition = transform.position;//pass by value hopefully
-		playerSetup.prevPosition = transform.position;//pass by value hopefully
-		//
-
+		playerSetup.prevPosition = transform.position;
 		isTouched = true;
+		pieceMoved = false;
 	}
 	
 	void OnMouseUp()
 	{
 		Debug.Log (playerSetup.activePage);
 		isTouched = false;
-		slot = findNearest();
+		oldScript = this.gameObject.GetComponentInParent<SetupTileScript> ();
+		nearestTile = findNearestTile();
 		//if there is a slot
-		Debug.Log (slot);
-		if (slot != null && !slot.GetComponent<SetupTileScript>().occupied)
+		Debug.Log (nearestTile);
+		if (nearestTile != null && !nearestTile.GetComponent<SetupTileScript>().occupied)
 		{
-			//Debug.Log ("unitType 10 or 11" + (gameObject.GetComponent<Unit>().unitType));
-			//Debug.Log ("7 or 8" + (gameObject.GetComponent<Unit>().unitRole));
-			//if(gameObject.GetComponent<Unit>().unitRole == 505 || gameObject.GetComponent<Unit>().unitRole == 506){
-			if(gameObject.GetComponent<Unit>().unitType == 10 || gameObject.GetComponent<Unit>().unitType == 11){
-				if(slot.GetComponent<SetupTileScript>().tt == SetupTileScript.TileType.ONFIELD)
-					{
-						playerSetup.pages[playerSetup.activePage].modified = true;
-						this.transform.parent.GetComponent<SetupTileScript>().occupied = false;
-						this.transform.parent = slot;
-						transform.position = new Vector3(slot.transform.position.x, 5.0f, slot.position.z);
-						slot.GetComponent<SetupTileScript>().occupied = true;
-					}
-				else
-					transform.position = playerSetup.prevPosition;
+			//guardian and soulstone must be kept on the field
+			if((gameObject.GetComponent<Unit>().unitType == 10 || gameObject.GetComponent<Unit>().unitType == 11) &&
+				nearestTile.GetComponent<SetupTileScript>().tt == SetupTileScript.TileType.ONFIELD)
+			{
+				pieceMoved = true;
+				onField = true;
 			}
 
+			//if there is room on the board for units...
 			else if(playerSetup.pages[playerSetup.activePage].onBoardPieces.Count < playerSetup.boardCapacity)
 			{
-				if(slot.GetComponent<SetupTileScript>().tt == SetupTileScript.TileType.ONFIELD)
+				//...and the unit is being placed on the field...
+				if(nearestTile.GetComponent<SetupTileScript>().tt == SetupTileScript.TileType.ONFIELD)
 				{
+					//...only add the unit if it is not already on the board
 					if(!playerSetup.pages[playerSetup.activePage].onBoardPieces.Contains(this.gameObject))
 					{
-
-						Debug.Log ("piece added. page " + playerSetup.activePage);
 						playerSetup.pages[playerSetup.activePage].offBoardPieces.Remove(this.gameObject);
 						playerSetup.pages[playerSetup.activePage].onBoardPieces.Add(this.gameObject);
 					}
 
-					playerSetup.pages[playerSetup.activePage].modified = true;
-					this.transform.parent.GetComponent<SetupTileScript>().occupied = false;
-					this.transform.parent = slot;
-					transform.position = new Vector3(slot.transform.position.x, 5.0f, slot.position.z);
-					slot.GetComponent<SetupTileScript>().occupied = true;
+					pieceMoved = true;
+					onField = true;
 				}
 
 			
-				else 
+				else //unit is being placed off the field
 				{
+					//if the unit is being moved off the field (rather than being adjusted off the board)
 					if(playerSetup.pages[playerSetup.activePage].onBoardPieces.Contains(gameObject))
 					{
 						playerSetup.pages[playerSetup.activePage].offBoardPieces.Add(this.gameObject);
 						playerSetup.pages[playerSetup.activePage].onBoardPieces.Remove(this.gameObject);
 					}
 
-					playerSetup.pages[playerSetup.activePage].modified = true;
-					this.transform.parent.GetComponent<SetupTileScript>().occupied = false;
-					this.transform.parent = slot;
-					transform.position = new Vector3(slot.transform.position.x, 5.0f, slot.position.z);
-					slot.GetComponent<SetupTileScript>().occupied = true;
+					pieceMoved = true;
+					onField = false;
 				}
 			}
 
-			else
+			else //board is at max capacity
 			{
-				if(slot.GetComponent<SetupTileScript>().tt == SetupTileScript.TileType.ONFIELD)
+				if(nearestTile.GetComponent<SetupTileScript>().tt == SetupTileScript.TileType.ONFIELD)
 				{
-					//	Debug.Log (playerSetup.pages[playerSetup.activePage].onBoardPieces.Contains(gameObject));
+					//the piece can only be moved in the case that it is already on the board and is being adjusted
 					if(playerSetup.pages[playerSetup.activePage].onBoardPieces.Contains(gameObject))
 					{
-						playerSetup.pages[playerSetup.activePage].modified = true;
-						this.transform.parent.GetComponent<SetupTileScript>().occupied = false;
-						this.transform.parent = slot;
-						transform.position = new Vector3(slot.transform.position.x, 5.0f, slot.position.z);
-						slot.GetComponent<SetupTileScript>().occupied = true;
+						pieceMoved = true;
+						onField = true;
 					}
-
-					else
-						transform.position = playerSetup.prevPosition;
-
 				}
 
-				else if(playerSetup.pages[playerSetup.activePage].onBoardPieces.Contains(gameObject))
+				else //offfield - player is moving a piece when the board is full
 				{
-					playerSetup.pages[playerSetup.activePage].offBoardPieces.Add(this.gameObject);
-					playerSetup.pages[playerSetup.activePage].onBoardPieces.Remove(this.gameObject);
+					//the piece being moved off the field
+					if(playerSetup.pages[playerSetup.activePage].onBoardPieces.Contains(gameObject))
+					{
+						playerSetup.pages[playerSetup.activePage].offBoardPieces.Add(this.gameObject);
+						playerSetup.pages[playerSetup.activePage].onBoardPieces.Remove(this.gameObject);
+					}
 
-					playerSetup.pages[playerSetup.activePage].modified = true;
-					this.transform.parent.GetComponent<SetupTileScript>().occupied = false;
-					this.transform.parent = slot;
-					transform.position = new Vector3(slot.transform.position.x, 5.0f, slot.position.z);
-					slot.GetComponent<SetupTileScript>().occupied = true;
+					pieceMoved = true;
+					onField = false;
 				}
 			}
 
-
-			//Special if dragging object to field-type tile:
-			//Debug.Log(playerSetup.activePage);
 			Debug.Log(playerSetup.pages[playerSetup.activePage].onBoardPieces.Count);
-
-
-
-
 		}
-		else
-		
+
+		if (pieceMoved) 
 		{
+			int oldX = oldScript.x;
+			int oldY = oldScript.y;
 
-			//transform.position = PlayerSetup.prevPosition;
-			transform.position = playerSetup.prevPosition;
+			int newX = nearestTile.GetComponent<SetupTileScript>().x;
+			int newY = nearestTile.GetComponent<SetupTileScript>().y;
 
+			int unitType = this.gameObject.GetComponent<Unit>().unitType;
+
+			gp.returnSocket().SendTCPPacket("movePiece\\"+ gp.playerName + "\\" + playerSetup.activePage + "\\"
+			                                + unitType + "\\" + oldX + "\\" + oldY + "\\" + newX + "\\" + newY + "\\" + onField);
+
+			this.gameObject.GetComponentInParent<SetupTileScript>().occupied = false;
+			this.gameObject.transform.parent = nearestTile.transform;
+			transform.position = new Vector3(nearestTile.transform.position.x, 5.0f, nearestTile.transform.position.z);
+			nearestTile.GetComponent<SetupTileScript>().occupied = true;
 		}
-
-
-
-
-
+		else		
+		{
+			transform.position = playerSetup.prevPosition;
+		}
 	}
-	
-	Transform findNearest()
+
+	GameObject findNearestTile()
 	{
 		float nearestDistanceTile = Mathf.Infinity;
 		GameObject[] taggedGameObjects = GameObject.FindGameObjectsWithTag(tagName);//<---KEY compares all objects near with that tag name!  Works perfectly for a set of tiles!!!
-		Transform nearestObj = null;
+		GameObject nearestObj = null;
 		foreach (GameObject obj in taggedGameObjects)
 		{
 			Vector3 objectPos = obj.transform.position;
@@ -166,17 +150,13 @@ public class move : MonoBehaviour {
 			
 			if (distanceTile < nearestDistanceTile)
 			{
-				nearestObj = obj.transform;
+				nearestObj = obj;
 				nearestDistanceTile = distanceTile;
 			}
 		}
 		
 		return nearestObj;
 	}
-
-
-
-
 	
 	void Update()
 	{
