27d73b479a51ef0cd04ffcaad80e37bf00d7dd99
joester <joey_shepard@sbcglobal.net>
2014-12-11 17:39:01 -0800

    changes
    yes

--- Guardians of the Arena/Assets/setTransparencyScript.cs
+++ Guardians of the Arena/Assets/setTransparencyScript.cs
@@ -6,6 +6,8 @@ public class setTransparencyScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		gameObject.GetComponent<SpriteRenderer> ().material.shader = Shader.Find ("Unlit/Transparent");
+		transform.localScale = new Vector3 (1.25f, 12.5f, 1.25f);
+		transform.Rotate(new Vector3 (4.5f, 0f, 0f));
 	}
 	
 	// Update is called once per frame
