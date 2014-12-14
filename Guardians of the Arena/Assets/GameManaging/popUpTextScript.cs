b05aef14d02b030b1c008db1a8bcb985b03e31c5
Steven Kawafuchi <13loodySword@gmail.com>
2014-12-14 15:41:48 -0800

    Fly away popup text Fly away
    Made popup text go up as it fades

--- Guardians of the Arena/Assets/GameManaging/popUpTextScript.cs
+++ Guardians of the Arena/Assets/GameManaging/popUpTextScript.cs
@@ -10,11 +10,19 @@ public class popUpTextScript : MonoBehaviour {
 	
 	public IEnumerator showText(Unit unitAffected,string affect, Color newColor){
 		GUI.depth = -1;
+		Vector3 pos = this.transform.position;
+
 		this.GetComponent<GUIText>().text = affect;
 		this.GetComponent<GUIText>().color = newColor;
 		Color temp = this.GetComponent<GUIText>().material.color;
-		yield return new WaitForSeconds(2.0f);
+		for (int i =0; i < 40; i ++) {
+			pos.y += 0.0005f;
+			this.transform.position = pos;
+			yield return new WaitForSeconds (0.016666f);
+		}
 		for (int i =0; i < 60; i ++){
+			pos.y += 0.0005f;
+			this.transform.position = pos;
 			temp.a -= 0.016f;
 			this.GetComponent<GUIText>().material.color = temp;
 			yield return new WaitForSeconds(0.016666f);
