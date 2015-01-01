bd0401149bf9e4489c6dcaa30b4e218d589dbdeb
JordanLingHua <jordan.hua@yahoo.com>
2015-01-01 00:51:42 -0800

    in progress

---
+++ Guardians of the Arena/Assets/CharacterObjects/AnimateGuardian.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This script is attached to prefab and will work like AnimateOnAction
+
+public class AnimateGuardian : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Guardians of the Arena/Assets/CharacterObjects/AnimateOnAction.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class AnimateOnAction : MonoBehaviour {
+
+	public string sheetname;
+	private Sprite[] sprites;
+	private SpriteRenderer sr;
+	private string[] names;
+
+	// Use this for initialization
+	void Start () {
+
+		sprites = (UnityEngine.Sprite[])Resources.LoadAll(sheetname); 
+		sr = GetComponent<SpriteRenderer> ();
+		names = new string[sprites.Length];
+		
+		for(int i = 0; i < names.Length; i++) 
+		{
+			names[i] = sprites[i].name;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	void ChangeSprite( int index )
+	{
+		Sprite sprite = sprites[index];
+		sr.sprite = sprite;
+	}
+	
+	void ChangeSpriteByName( string name )
+	{
+		Sprite sprite = sprites[getIndexOf(names, name)];
+		sr.sprite = sprite;
+	}
+
+
+	int getIndexOf(string[] names, string value)
+	{
+
+		for(int i = 0; i < names.Length; i++)
+		{
+			if(value == names[i])
+			{
+				return i;
+			}
+		}
+
+		//If no match found
+		return 0;
+
+	}
+
+}
