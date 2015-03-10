04b931e284b47857d882dd95d3610290254ba5ba
joester <joey_shepard@sbcglobal.net>
2015-03-09 17:55:18 -0700

    fixed extra archer in setup 1
    need to rebuild the server to see the change

--- Guardians Of The Arena Server/Guardians Of The Arena Server/DataManager.cs
+++ Guardians Of The Arena Server/Guardians Of The Arena Server/DataManager.cs
@@ -378,7 +378,7 @@ namespace Guardians_Of_The_Arena_Server
             sql = "   INSERT INTO unitSetups  ";
             sql += "   (name, setupName, setupID, unitType, x , y, onField)    ";
             sql += "   VALUES  ";
-            sql += "   ('" + name + "', '" + pageName + "'," + pageNumber + ",1 , 4, 0, 0)";
+            sql += "   ('" + name + "', '" + pageName + "'," + pageNumber + ",8 , 4, 0, 0)";
             command = new SQLiteCommand(sql, userDatabase);
             command.ExecuteNonQuery();
 
